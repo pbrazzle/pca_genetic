48e72cd43e023951bf313a28662b68305212d0e5
pbrazzle <prb50@pitt.edu>
2023-03-22 11:36:22 -0400

    Implemented DownSamplingModel

--- src/model/downsample/DownSamplingModel.hpp
+++ src/model/downsample/DownSamplingModel.hpp
@@ -8,7 +8,19 @@ namespace GeneticModels
 	//TODO implement this
 	class DownSamplingModel : public GeneticModel
 	{
+		private:
+			std::pair<int, int> pool_size;
+			std::pair<int, int> image_size;
+
+			std::vector<std::vector<double>> reorderByPools(const std::vector<double>&);
+			std::vector<double> maxPool(const std::vector<std::vector<double>>&);
+			std::vector<double> getPool(const std::vector<double>&, int);
+
 		public:
+			DownSamplingModel() = default;
+			DownSamplingModel(int, int, int, int);
+			DownSamplingModel(std::pair<int, int>, std::pair<int, int>);
+
 			std::unique_ptr<GeneticModel> clone() const;
 			
 			std::unique_ptr<ModelOutputData> evaluate(ModelInputData&);
